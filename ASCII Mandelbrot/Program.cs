1fd0148bd71f8ef01d6fc879a2391ad1f2f52bf6
Nathan O'Connor <nathanoconnor2209@gmail.com>
2022-06-14 19:39:23 +0100

    functional

--- ASCII Mandelbrot/Program.cs
+++ ASCII Mandelbrot/Program.cs
@@ -1,45 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Timers;
+using System.Diagnostics;
 
 namespace ASCII_Mandelbrot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // TODO: Abstract all of this out of main with separate functions
         {
+            Console.WriteLine("(Fullscreen recommended)");
             Console.WriteLine("Enter initial frame width");
             decimal initialWidth = decimal.Parse(Console.ReadLine());
             Console.WriteLine("Enter final frame width");
             decimal finalWidth = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter X coordinate to zoom to");
+            decimal finalCentreX = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y coordinate to zoom to");
+            decimal finalCentreY = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter maximum iteration count");
+            int maxIterations = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter animation duration");
             float duration = float.Parse(Console.ReadLine());
 
-            List<char> frame = MandelbrotGenerator.GenerateMandelbrot(
-                width: 80,
-                height: 80,
-                xWidth: 4,
-                yWidth: 4,
-                centrePosX: 0,
-                centrePosY: 0,
-                iterations: 500
-            );
+            Stopwatch totalElapsedSW = new Stopwatch();
+            Stopwatch deltaTimeSW = new Stopwatch();
 
-            string frameStr = "";
-            foreach (char c in frame)
+            totalElapsedSW.Start();
+            deltaTimeSW.Start();
+
+            int framesPrinted = 0;
+            bool finalFrameDrawn = false;
+            while (totalElapsedSW.ElapsedMilliseconds < duration * 1000 || finalFrameDrawn == false)
             {
-                if (c != '\n')
+                deltaTimeSW.Restart();
+
+                long functionalElapsedMilliseconds = totalElapsedSW.ElapsedMilliseconds;   // So that the final frame drawn is precisely as wanted
+                if (totalElapsedSW.ElapsedMilliseconds > duration * 1000)
                 {
-                    frameStr += c;
-                    frameStr += c;
+                    functionalElapsedMilliseconds = (long)duration * 1000;
+                    finalFrameDrawn = true;
                 }
 
-                else
+                List<char> frame = MandelbrotGenerator.GenerateMandelbrot(
+                    width: 80,
+                    height: 80,
+                    xWidth: (decimal)((double)initialWidth * Math.Pow((double)(finalWidth / initialWidth), functionalElapsedMilliseconds / (float)1000 / duration)),
+                    yWidth: (decimal)((double)initialWidth * Math.Pow((double)(finalWidth / initialWidth), functionalElapsedMilliseconds / (float)1000 / duration)),
+                    centrePosX: finalCentreX,
+                    centrePosY: finalCentreY,
+                    iterations: maxIterations
+                );
+
+                string frameStr = "";
+                foreach (char c in frame)
                 {
-                    frameStr += c;
+                    if (c != '\n')
+                    {
+                        frameStr += c;
+                        frameStr += c;  // Add twice to make up for character rectangularity
+                    }
+
+                    else
+                    {
+                        frameStr += c;
+                    }
                 }
+                Console.SetCursorPosition(0, 0);
+                Console.Write(frameStr);
+                
+                Console.WriteLine($"\n{Math.Round(1000f / deltaTimeSW.ElapsedMilliseconds)} FPS   "); // Maybe move this into a separate function i.e. DisplayFPS()
+                
+                framesPrinted++;
             }
-            Console.Write(frameStr);
+            Console.WriteLine("Finished");
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
     }
@@ -90,7 +124,7 @@ namespace ASCII_Mandelbrot
 
                     if (Zx2 + Zy2 < 4)
                     {
-                        pixels.Add('@');
+                        pixels.Add('.');
                     }
 
                     else
